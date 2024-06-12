5c57fdfb9522f0694a063136726af216ed32e4fa
AmanPunia-510 <puniaaman888@gmail.com>
2024-06-12 11:14:10 +0530

    work on project is completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -60,28 +60,8 @@ accordian_items.forEach((items) => {
   });
 });
 
-const item = document.querySelector(".navbar-items");
-const menu = document.querySelector(".toggle");
+const preloader = document.querySelector(".preloader");
 
-menu.addEventListener("click", () => {
-  item.classList.toggle("show");
-  let contain = document.querySelector("body");
-  let index = document.querySelector(".js-z");
-
-  if (item.classList.contains("show")) {
-    contain.style.position = "fixed";
-    contain.style.top = "0";
-    contain.style.left = "0";
-    contain.style.width = "100%";
-    index.classList.add("z-0");
-    index.classList.remove("z-2");
-  } else {
-    contain.style.position = "unset";
-
-    setTimeout(() => {
-      index.classList.remove("z-0");
-      index.classList.add("z-2");
-    }, 200);
-  }
-});
-
+setTimeout(() => {
+  preloader.classList.add("d-none");
+}, 3000);
